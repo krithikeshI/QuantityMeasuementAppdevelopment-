0c25317ce66e18fbc8b82d6cfe0adc6efb7b3cd1
KrithikeshI <pkrithikesh4@gmail.com>
2026-04-24 15:37:12 +0530

    [KrithikeshI] Added [feature/UC4]

--- App/src/main/QCMGMT_APP.java
+++ App/src/main/QCMGMT_APP.java
@@ -3,8 +3,10 @@ package main;
 public class QCMGMT_APP {
 
     public enum LengthUnit {
-        FEET(1.0),
-        INCH(1.0 / 12.0); // 12 inches = 1 foot
+        INCH(1.0),
+        FEET(12.0),
+        YARDS(36.0),
+        CENTIMETERS(0.393701);
 
         private final double conversionFactor;
 
@@ -55,9 +57,14 @@ public class QCMGMT_APP {
     }
 
     public static void main(String[] args) {
-        QuantityLength q1 = new QuantityLength(1.0, LengthUnit.FEET);
-        QuantityLength q2 = new QuantityLength(12.0, LengthUnit.INCH);
 
-        System.out.println(q1.equals(q2)); // true
+        QuantityLength yard = new QuantityLength(1, LengthUnit.YARDS);
+        QuantityLength feet = new QuantityLength(3, LengthUnit.FEET);
+
+        QuantityLength cm = new QuantityLength(1, LengthUnit.CENTIMETERS);
+        QuantityLength inch = new QuantityLength(0.393701, LengthUnit.INCH);
+
+        System.out.println(yard.equals(feet)); // true
+        System.out.println(cm.equals(inch));   // true
     }
 }
